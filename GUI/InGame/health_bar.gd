8a12617a59b10afabe0066cfee5e2fb26798de2b
Scott Petrovic <scottpetrovic@gmail.com>
2025-01-28 16:17:14 -0600

    Add in healthbar progress art and hook up

---
+++ GUI/InGame/health_bar.gd
@@ -0,0 +1,18 @@
+extends Control
+
+@onready var health_bar: Sprite2D = $HealthBar
+
+var health_bar_starting_width: float = 0
+var player_reference: HelicopterPlayer = null
+
+func _ready() -> void:
+	player_reference = Constants.player_reference
+	health_bar_starting_width = health_bar.region_rect.size.x
+
+
+func _process(delta: float) -> void:
+	# update the healthbar region's width based of player heatlh
+	var health_perc: float = float(player_reference.health) / float(player_reference.max_health)
+	health_bar.region_rect.size.x = health_perc * health_bar_starting_width
+	
+	
--- GUI/gui_health.gd
+++
@@ -1,5 +0,0 @@
-extends Label
-
-func _process(delta: float) -> void:
-	text = 'Health:' + str(Constants.player_reference.health) + '/'
-	text += str(Constants.player_reference.max_health)
