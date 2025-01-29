ad5aa8c6247ba825d8880d93404c76abc0d138a0
Scott Petrovic <scottpetrovic@gmail.com>
2025-01-29 08:41:50 -0600

    Add gravity to the enemy bullet

--- Objects/EnemyBullet/enemy_bullet.gd
+++ Objects/EnemyBullet/enemy_bullet.gd
@@ -2,35 +2,31 @@ class_name EnemyBullet
 extends Area2D
 
 @onready var lifetime_timer: Timer = $LifetimeTimer
-const BULLET_SPEED = 50
-
+const BULLET_SPEED = 120
+const GRAVITY = 200  # Adjust this value to control gravity strength
+var velocity: Vector2
 var _rotation: float = 0
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
 	add_to_group("EnemyBullet")
-	
 	body_entered.connect(_body_entered)
 	
-	lifetime_timer.wait_time = 1.5
+	lifetime_timer.wait_time = 1.0
 	lifetime_timer.one_shot = true
-	lifetime_timer.timeout.connect( func(): queue_free() )
+	lifetime_timer.timeout.connect(func(): queue_free())
 	lifetime_timer.start()
-
+	
+	# Initialize the velocity based on rotation and speed
+	velocity = Vector2.RIGHT.rotated(rotation) * BULLET_SPEED
 
 func _body_entered(body: Node2D) -> void:
-	
-	#print(body.name)
 	if body.has_method("hit"):
 		body.hit()
-	
 	queue_free()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	# just shoot straight locally. We should already be rotated
-	var direction = Vector2.RIGHT.rotated(rotation)
-	position += direction * BULLET_SPEED * delta
-	pass
+	# Apply gravity to the velocity
+	velocity.y += GRAVITY * delta
+	
+	# Move the bullet using the velocity
+	position += velocity * delta
