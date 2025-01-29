a778f8e8d7928fce35ba12728066f4183fe04787
Scott Petrovic <scottpetrovic@gmail.com>
2025-01-29 08:38:09 -0600

    Logic so prisoner faces the right direction when returning to base

--- Objects/Prisoner/prisoner.gd
+++ Objects/Prisoner/prisoner.gd
@@ -6,19 +6,14 @@ extends Area2D
 
 var _distance_to_player: float = 0.0
 
+# what is on the prisoner's mind. They either need to get in the helicopter
+# of they need to get to the base HQ to be saved
 var _objective: Constants.PrisonerObjective = Constants.PrisonerObjective.GET_IN_HELICOPTOR
 
-enum PrisonerBehaviorState {
-	IDLE,
-	WALK
-}
+enum PrisonerBehaviorState { IDLE, WALK }
 var prisoner_state: PrisonerBehaviorState = PrisonerBehaviorState.IDLE
 
-
-enum PrisonerDirection {
-	LEFT,
-	RIGHT
-}
+enum PrisonerDirection { LEFT, RIGHT }
 var prisoner_facing_direction: PrisonerDirection = PrisonerDirection.LEFT
 
 func set_objective(obj: Constants.PrisonerObjective) -> void:
@@ -27,6 +22,7 @@ func set_objective(obj: Constants.PrisonerObjective) -> void:
 func _process(delta: float) -> void:
 	
 	prisoner_state = PrisonerBehaviorState.IDLE
+	_update_facing_direction()
 	
 	# the base is always to the right, so run that
 	if _objective == Constants.PrisonerObjective.GET_IN_BASE:
@@ -45,19 +41,27 @@ func _process(delta: float) -> void:
 			Constants.prisoner_captured()
 			queue_free()
 
+func _update_facing_direction() -> void:
+	# base HQ is always on right side for now
+	if _objective == Constants.PrisonerObjective.GET_IN_BASE:
+		prisoner_facing_direction = PrisonerDirection.RIGHT
+	
+	# face direction of helicopter
+	if _objective == Constants.PrisonerObjective.GET_IN_HELICOPTOR:		
+		if _direction_towards_player().x > 0:
+			prisoner_facing_direction = PrisonerDirection.RIGHT
+		else:
+			prisoner_facing_direction = PrisonerDirection.LEFT
+
+
+func _direction_towards_player() -> Vector2:
+	return (Constants.player_reference.global_position - global_position).normalized()
+
 
 func walk_toward_player(delta: float) -> void:
 	# If player is landed and close enough, move towards them
 	if Constants.player_reference.is_on_floor() and _distance_to_player < distance_to_watch_for_player:  # 50 pixels, adjust as needed
-		# Get direction to player
-		var direction = (Constants.player_reference.global_position - global_position).normalized()
-		
 		# Move towards player
-		global_position.x += direction.x * move_speed * delta
-		
+		global_position.x += _direction_towards_player().x * move_speed * delta
 		prisoner_state = PrisonerBehaviorState.WALK
 		
-		if direction.x > 0:
-			prisoner_facing_direction = PrisonerDirection.RIGHT
-		else:
-			prisoner_facing_direction = PrisonerDirection.LEFT
