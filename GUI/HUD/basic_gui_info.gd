efb6741674ee65775099e649bd95760ee82e59bd
Scott Petrovic <scottpetrovic@gmail.com>
2025-01-30 20:01:07 -0600

    Show minimum prisoners needed for level complete. Update top HUD with icons

--- GUI/HUD/basic_gui_info.gd
+++ GUI/HUD/basic_gui_info.gd
@@ -1,8 +1,13 @@
-extends Label
+extends Control
+
+@onready var remaining_prisoners: Label = $RemainingPrisoners
+@onready var prisoners_in_helicopter: Label = $PrisonersInHelicopter
+@onready var prisoners_saved: Label = $PrisonersSaved
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	text = "A:" + str(Constants.level_total_remaining_prisoners)
-	text += "  B:" + str(Constants.player_reference.prisoners_in_helicopter)
-	text += "  C:" + str(Constants.level_total_prisoners_saved)
+	remaining_prisoners.text = str(Constants.level_total_remaining_prisoners)
+	prisoners_in_helicopter.text = str(Constants.player_reference.prisoners_in_helicopter)
+	prisoners_saved.text = str(Constants.level_total_prisoners_saved)
+	
--- Scenes/LevelObjectives/level_objectives.gd
+++ Scenes/LevelObjectives/level_objectives.gd
@@ -2,7 +2,8 @@ extends VBoxContainer
 
 @onready var current_level: Label = $CurrentLevel
 @onready var lives_left: Label = $LivesLeft
-@onready var prisoners_left: Label = $PrisonersLeft
+@onready var prisoners_required: Label = $PrisonersRequired
+
 
 @onready var level_objectives: CanvasLayer = $".."
 
@@ -20,7 +21,9 @@ func show_screen_on_level_fail() -> void:
 func _process(_delta: float) -> void:
 	current_level.text = 'LEVEL ' + str(Constants.current_level+1)
 	lives_left.text = ' x ' + str(Constants.lives_left).pad_zeros(2)
-	prisoners_left.text = ' x ' + str(Constants.level_total_remaining_prisoners).pad_zeros(2)
+	
+	var min_prisoners = Constants.level_data[Constants.current_level].minimum_prisoners_needed
+	prisoners_required.text = ' x ' + str(min_prisoners).pad_zeros(2)
 	
 	if Input.is_action_just_pressed("shoot"):
 		level_objectives.visible = false
