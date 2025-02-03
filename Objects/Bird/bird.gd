9551f1064b8ca8b53ab199fd4f625889956b372b
Scott Petrovic <scottpetrovic@gmail.com>
2025-02-03 16:42:01 -0600

    have birds go side to side

--- Objects/Bird/bird.gd
+++ Objects/Bird/bird.gd
@@ -5,6 +5,8 @@ extends Area2D
 @export var frequency: float = 0.5    # How fast the object moves
 var time: float = 0.0                 # Time tracker for the sine wave
 var initial_y: float                  # Store the starting X position
+var initial_x: float
+var x_distance: float = 30.0
 
 var health: int = 2
 
@@ -14,6 +16,7 @@ func _ready():
 	
 	# Store the initial x position when the node starts
 	initial_y = position.y
+	initial_x = position.x
 	
 	body_entered.connect(_body_enter)
 
@@ -34,13 +37,20 @@ func _body_enter(_body: Node2D) -> void:
 		player.hit()
 		hit(5) # this will kill the bird
 
+
 func _process(delta):
+	_move(delta)
+
+
+func _move(delta: float) -> void:
 	# Increment time
 	time += delta
 	
 	# Calculate the new x position using sine wave
 	# Sin function returns values between -1 and 1, which we multiply by amplitude
-	var offset = amplitude * sin(time * frequency)
+	var height_offset = amplitude * sin(time * frequency)
+	var width_movement = sin(time*0.1) * x_distance
 	
 	# Update only the x position, keeping the original y position
-	position.y = initial_y + offset
+	position.y = initial_y + height_offset
+	position.x = initial_x + width_movement
