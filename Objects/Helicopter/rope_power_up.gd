9de4d5c33c829cd5ff1527175ebc6641366f0dd8
Scott Petrovic <scottpetrovic@gmail.com>
2025-02-05 17:05:24 -0600

    Make rope ladder look nicer with animations.

--- Objects/Helicopter/rope_power_up.gd
+++ Objects/Helicopter/rope_power_up.gd
@@ -1,9 +1,8 @@
 extends Area2D
 
 var is_rope_colliding_with_something: bool = false
-@onready var sprite: ColorRect = $Sprite
+@onready var sprite: AnimatedSprite2D = $Sprite
 @onready var helicopter_player: HelicopterPlayer = $".."
-
 
 func _ready() -> void:
 	
@@ -31,7 +30,10 @@ func _body_exit(_body) -> void:
 func _process(delta: float) -> void:
 	
 	if Constants.level_active_powerup != Constants.PowerUpType.ROPE:
+		self.visible = false
 		return #exit out of process if we don't have a rope powerup
+	
+	self.visible = true
 
 	# add a bit of rotation to the rope, regardless of state
 	if helicopter_player.velocity.x < 0:
@@ -45,14 +47,21 @@ func _process(delta: float) -> void:
 	# the rope, extend it when we press the button
 	if Input.is_action_pressed("shoot_special") && \
 		is_rope_colliding_with_something == false:
-		_try_to_drop_rope_if_not()
+		_try_to_drop_rope_if_not(delta)
 	else:
-		_bring_in_rope_if_not()
+		_bring_in_rope_if_not(delta)
+
+func is_rope_completely_down() -> bool:
+	# if we are pressing down the special and our sprite is 
+	# all the way expanded, we are all the way out
+	return Input.is_action_pressed("shoot_special") && \
+		sprite.scale.y == 1.0
+
+func _try_to_drop_rope_if_not(delta: float) -> void:
+	if sprite.scale.y < 1.0:
+		sprite.scale.y = move_toward(sprite.scale.y, 1.0, delta*3)
 
 
-func _try_to_drop_rope_if_not() -> void:
-	sprite.visible = true
-
-
-func _bring_in_rope_if_not() -> void:
-	sprite.visible = false
+func _bring_in_rope_if_not(delta: float) -> void:
+	if sprite.scale.y > 0.0:
+		sprite.scale.y = move_toward(sprite.scale.y, 0.0, delta*3)
